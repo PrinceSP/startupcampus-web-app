93886a668afd8339817e06f25fc065863eaeb7c1
tioramadhn <tioramadhntio@apps.ipb.ac.id>
2022-09-13 07:43:56 +0700

    update testi2 content

--- content/testi2.js
+++ content/testi2.js
@@ -8,27 +8,43 @@ export const testi2 = [
     img: "/images/person.png",
   },
   {
-    title: "“Relevan dengan Dunia Nyata”",
+    title: "“Opportunity to develop skills”",
     story:
-      "Saya senang bisa bergabung di Startup Campus, di mana ini merupakan sebuah program yang baik untuk memerdekakan belajar para mahasiswa, tidak terpaku pada kurikulum resmi kuliah, dan lebih relevan untuk adaptasi dengan perubahan terkini di dunia nyata.",
-    name: "Ainun Najib",
-    role: "Head of Analytics, Platform, and Regional Business at Grab Singapore",
+      "Bored of typical campus learning? StartupCampus is a solution to guide you deepen your passion, unleash your potential and enrich your project portfolio.",
+    name: "Nolang Fanani",
+    role: "Computer Vision Specialist, Deputy Manager at Hyundai Mobis",
     img: "/images/person.png",
   },
   {
-    title: "“Relevan dengan Dunia Nyata”",
+    title: "“Wadah talent masa depan”",
     story:
-      "Saya senang bisa bergabung di Startup Campus, di mana ini merupakan sebuah program yang baik untuk memerdekakan belajar para mahasiswa, tidak terpaku pada kurikulum resmi kuliah, dan lebih relevan untuk adaptasi dengan perubahan terkini di dunia nyata.",
-    name: "Ainun Najib",
-    role: "Head of Analytics, Platform, and Regional Business at Grab Singapore",
+      "Saya sangat bersyukur bisa bergabung di Startup Campus dimana saya bisa melihat dan berkontribusi dalam mempersiapkan talent-talent masa depan Indonesia.",
+    name: "William Onnyxiforus",
+    role: "Softwareentwickler bei Labour Digital GmbH",
     img: "/images/person.png",
   },
   {
-    title: "“Relevan dengan Dunia Nyata”",
+    title: "“Penyederhanaan konsep kompleks”",
     story:
-      "Saya senang bisa bergabung di Startup Campus, di mana ini merupakan sebuah program yang baik untuk memerdekakan belajar para mahasiswa, tidak terpaku pada kurikulum resmi kuliah, dan lebih relevan untuk adaptasi dengan perubahan terkini di dunia nyata.",
-    name: "Ainun Najib",
-    role: "Head of Analytics, Platform, and Regional Business at Grab Singapore",
+      "Perjalanan yang intens dengan para mentee. Hal ini mengajarkan saya untuk dapat menyederhanakan konsep yang kompleks sehingga mentee dapat dengan mudah memahami dan mengimplementasikan konsep tersebut.",
+    name: "Pradita Astarina",
+    role: "SVP of Investment and Business Development at Cakra Finansindo Investama",
+    img: "/images/person.png",
+  },
+  {
+    title: "“Startup yang luar biasa”",
+    story:
+      "Tidak peduli seberapa sama, masing-masing dari mereka membawa sesuatu yang berbeda. Saya sangat menghargai kesempatan sebagai mentor untuk berkontribusi dalam membentuk para founder startup yang luar biasa.",
+    name: "Mushfi Ridho",
+    role: "AVP Supply Chain FInancing at Pintek ",
+    img: "/images/person.png",
+  },
+  {
+    title: "“Rekomendasi untuk pemula”",
+    story:
+      "Menjadi mentor di Startup Campus adalah pengalaman yang seru, menantang, dan menambah wawasan. Saya  juga mendapat banyak pengetahuan, networks dan perspektif baru dalam proses pengembangan bisnis.",
+    name: "Nur Faridah",
+    role: "Senior Business Operations Manager at Ruangguru",
     img: "/images/person.png",
   },
 ];
