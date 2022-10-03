226d5e24542816f71848e6b813b22d0b1e40f865
PrinceSP <princepasombaran@gmail.com>
2022-10-04 06:03:49 +0800

    combine all the export sections

---
+++ components/homeSection/index.js
@@ -0,0 +1,12 @@
+import Section1 from './Section1.js'
+import Section2 from './Section2.js'
+import Section3 from './Section3.js'
+import Section4 from './Section4.js'
+import Section5 from './Section5.js'
+import Section6 from './Section6.js'
+import Section7 from './Section7.js'
+import Section8 from './Section8.js'
+import Section9 from './Section9.js'
+import Section10 from './Section10.js'
+
+export {Section1,Section2,Section3,Section4,Section5,Section6,Section7,Section8,Section9,Section10}
