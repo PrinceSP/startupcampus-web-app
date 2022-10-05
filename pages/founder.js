a735111d192bb802584397ab4e35569de37527a7
PrinceSP <princepasombaran@gmail.com>
2022-10-05 22:48:34 +0800

    add section three

--- pages/founder.js
+++ pages/founder.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {SectionFounder1,SectionFounder2} from '../components'
+import {SectionFounder1,SectionFounder2,SectionFounder3} from '../components'
 import Head from "next/head";
 
 const Founder = () => {
@@ -10,6 +10,7 @@ const Founder = () => {
       </Head>
       <SectionFounder1/>
       <SectionFounder2/>
+      <SectionFounder3/>
     </>
   )
 }
