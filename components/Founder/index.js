a4c4f361677212f273cb6fd4b825473fd6b560c6
PrinceSP <princepasombaran@gmail.com>
2022-10-05 22:51:18 +0800

    export section three

--- components/Founder/index.js
+++ components/Founder/index.js
@@ -1,4 +1,5 @@
 import SectionFounder1 from './Section1.js'
 import SectionFounder2 from './Section2.js'
+import SectionFounder3 from './Section3.js'
 
-export { SectionFounder1,SectionFounder2}
+export { SectionFounder1,SectionFounder2,SectionFounder3}
