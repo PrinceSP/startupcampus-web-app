4d76044de84bc55de2742997951d792aa002134c
PrinceSP <princepasombaran@gmail.com>
2022-10-07 17:44:27 +0800

    add karya section

--- components/Founder/index.js
+++ components/Founder/index.js
@@ -7,5 +7,6 @@ import SectionFounder6 from './Section6.js'
 import SectionFounder7 from './Section7.js'
 import SectionFounder8 from './Section8.js'
 import SectionFounder9 from './Section9.js'
+import SectionFounder10 from './Section10.js'
 
-export { SectionFounder1,SectionFounder2,SectionFounder3,SectionFounder4,SectionFounder5,SectionFounder6,SectionFounder7,SectionFounder8,SectionFounder9}
+export { SectionFounder1,SectionFounder2,SectionFounder3,SectionFounder4,SectionFounder5,SectionFounder6,SectionFounder7,SectionFounder8,SectionFounder9,SectionFounder10}
